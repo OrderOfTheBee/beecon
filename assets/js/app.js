51314b0019c8d4a0cbe6a01005ef10b3a04be283
Oksana Kurysheva <okurysheva@gmail.com>
2016-04-03 01:03:32 +0300

    First version of agenda

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', [ 'ui.bootstrap' , 'ngSanitize']);
+var app = angular.module('app', [ 'ui.bootstrap' , 'ngSanitize', 'jshor.angular-addtocalendar']);
 
 app.controller('SpeakersCtrl', ['$scope', '$http', '$timeout', '$modal', function($scope, $http, $timeout, $modal, $log){
 	$scope.speakers = [];
@@ -42,6 +42,107 @@ app.controller('TalksCtrl', ['$scope', '$http', '$location', '$filter',  '$timeo
       $scope.talks = data;
     })
     $scope.open = function (_speaker) {
+    var modalInstance = $modal.open({
+      controller: "ModalInstanceCtrl",
+      templateUrl: 'SpeakerModalContent.html',
+      scope: $scope,
+      resolve: {
+        item: function()
+        {
+          $scope.currentItem = _speaker;
+        }
+      }
+    });
+  };
+}]);
+
+app.controller('AgendaCtrl', ['$scope', '$http', '$location', '$filter',  '$timeout', '$modal', function($scope, $http, $location, $filter,  $timeout, $modal){
+  $scope.agenda = [];
+  $http.get('/assets/data/agenda.json').success(function(data) {
+    var array = data;
+    var date, startDate, endDate;
+    $.each(array, function(index, day) {
+      date = "201604" + day.date.substr(day.date.length - 2) + "T";
+      $.each(day.slots, function(index2, slot) {
+        var hour = slot.time.split(' ')[0].split(':')[0];
+        if (hour.length == 1) { 
+          hour = "0" + hour;
+        }
+        startDate = date + hour + slot.time.split(' ')[0].split(':')[1];
+        var hour = slot.time.split(' ')[2].split(':')[0];
+        if (hour.length == 1) { 
+          hour = "0" + hour;
+        }
+        endDate = date + hour + slot.time.split(' ')[2].split(':')[1];
+        $.each(slot.sessions, function(index3, session) {
+          session.startDate = startDate + "00";
+          if (session["length"] == 2) {
+            var hour = day.slots[index2+1].time.split(' ')[2].split(':')[0]
+            if (hour.length == 1) { 
+              hour = "0" + hour;
+            }
+            var endDate2 = date + hour + day.slots[index2+1].time.split(' ')[2].split(':')[1] + "00";
+            session.endDate = endDate2;
+          } else {
+            session.endDate = endDate + "00";
+          }
+        });
+      });
+    });
+
+
+    $http.get('/assets/data/talks.json').success(function(data) {
+      var talks = data;
+      $.each(array, function(index, day) {
+        $.each(day.slots, function(index2, slot) {
+          $.each(slot.sessions, function(index3, session) {
+            if (session.id[0] == "2") {
+              var i = talks.filter(function(e) { return e.id == session.id; });
+              session.title = i[0].title;
+              session.speakers = [];
+              session.speakers = i[0].speakers.slice(0);
+              session.abstract = i[0].abstract;
+              session.type = i[0].type;
+              session.topic = i[0].topic;
+            } else {
+              session.title = session.id;
+            }
+          })
+        })
+      })
+    });
+    $http.get('/assets/data/speakers.json').success(function(data) {
+      var speakers = data;
+      $.each(array, function(index, day) {
+        $.each(day.slots, function(index2, slot) {
+          $.each(slot.sessions, function(index3, session) {
+            if (session.id[0] == "2") {
+              $.each(session.speakers, function(index4, speaker) {
+                var i = speakers.filter(function(e) { return e.surname == speaker.surname; });
+                speaker.talks = i[0].talks.slice(0);
+              });
+            }
+          });
+        });
+      });
+    });
+    $scope.agenda = array;
+
+  });
+  $scope.open = function (_session) {
+    var modalInstance = $modal.open({
+      controller: "ModalInstanceCtrl",
+      templateUrl: 'SessionModalContent.html',
+      scope: $scope,
+      resolve: {
+        item: function()
+        {
+          $scope.currentItem = _session;
+        }
+      }
+    });
+  };
+  $scope.open2 = function (_speaker) {
     var modalInstance = $modal.open({
       controller: "ModalInstanceCtrl",
       templateUrl: 'SpeakerModalContent.html',
